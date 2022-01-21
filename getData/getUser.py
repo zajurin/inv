e588979a7ec552300f833c62f352bcd96c3cb938
Alan Rose <alan_rose@hotmail.es>
2022-01-20 22:02:34 -0600

    adding getUser.py and html to verify Azure questions

--- getData/getUser.py
+++ getData/getUser.py
@@ -1,68 +1,73 @@
+from distutils.util import change_root
+import subprocess as sp
 import re
 import time
-import subprocess as sp
+
+list_to_search = ["AzureAdJoined : ", "AzureAdPrt : ",
+                  "IsDeviceJoined : ", "IsUserAzureAD : "]
+username = sp.getoutput("whoami")
+checking_status = sp.getoutput("dsregcmd /status")
+my_model = sp.getoutput("wmic csproduct get name")
+mySerialNumber = sp.getoutput("wmic bios get serialnumber")
+
+# Cleaning unwanted ne lines
+theUsername = username.replace("\n", "")
+theModel = my_model.replace("\n", "")
+theSerialNumber = mySerialNumber.replace("\n", "")
+
+# Remove multiple spaces in strings
+cleanUserName = re.sub(" +", " ", theUsername)
+cleanModel = re.sub(" +", " ", theModel)
+cleanSerialNumber = re.sub(" +", " ", theSerialNumber)
+
+print(cleanUserName)
+print(cleanModel)
+print(cleanSerialNumber)
+print()
 
 
-class RunCommand:
+class MyCommand:
     def __init__(self, command):
         self.command = command
         global myResult
-        myResult = sp.run(self.command, capture_output=True, text=True)
-    # def getResult(self):
-    #     self.myResult = myResult
-        
-
-user = RunCommand('whoami')
-
-print(myResult.stdout)
-
-# brand = RunCommand('wmic csproduct get name')
-# model = RunCommand('wmic csproduct get name')
-# serialNumber = RunCommand('wmic bios get serialnumber')
-# macAddress = RunCommand('getmac /v')
-# ram = RunCommand('')
-# operatingSystem = RunCommand('')
-# licence = RunCommand('')
-# azureAdJoined = RunCommand('')
-# azureAdPrt = RunCommand('')
-# isDeviceJoined = RunCommand('')
-# isUserAzureAD = RunCommand('')
-
-# print(user, brand, model, serialNumber, ram, operatingSystem, licence, azureAdJoined, azureAdPrt, isDeviceJoined, isUserAzureAD, macAddress)
-
-# searched = "1394 OHCI Compliant Ho Kernel2"
-# def updating(myCommmand):
-#     myupdateCode = sp.getoutput(myCommmand)
-#     print(myupdateCode)
-
-# try:
-#     class RunDir:
-#         def __init__(self, command):
-#             self.command = command
-#             # global myResult
-#             myResult = sp.getoutput(self.command)
-#             print(myResult)
-#             if searched in myResult:
-#                 time.sleep(3)
-#                 print("I am waiting ...")
-#                 updating("dir")
-                   
-#             else:
-#                 whoisit = sp.getoutput("whoami")
-#                 print("I did not saw it")
-#                 print(f"in user {whoisit} ")
-
-#     # myUsername = RunDir('whoami')
-#     time.sleep(2)
-#     myDir = RunDir('driverquery')
-#     # myDir = RunDir('whoami')
-# except:
-#     print('Error')
+        myResult = sp.getoutput(self.command)
 
 
-# # print(myResult)
+def Runner():
+    repeat_n_times = 1
+    while repeat_n_times <= 2:
+        for eachSearched in list_to_search:
+            try:
+                myInstance = MyCommand("dsregcmd /status")
+                time.sleep(1)  # change "2" to "360" for 6 minutes
 
-# # AzureAdJoined : YES
-# # AzureAdPrt : YES
-# # IsDeviceJoined : YES
-# # IsUserAzureAD : YES
+                if eachSearched + "YES" in myResult:
+                    print(eachSearched + "YES")
+
+                elif eachSearched + "NO" in myResult:
+                    global changer
+                    changer = eachSearched + "NO"
+                    print(changer)
+
+                else:
+                    print("Nothing was found it")
+            except:
+                print("error")
+        repeat_n_times += 1
+        if repeat_n_times < 3 and changer:
+            print(
+                "\nSeems that one or more of the values was 'NO'.\nWe need to update this process ")
+            print()
+            updating = sp.getoutput("gpupdate /force")
+            print(updating)
+            minutes_to_wait = .5
+            myTime = minutes_to_wait = 60
+            print(f"Waiting {minutes_to_wait} minutes")
+            timeInSeconds = time.sleep(myTime)
+            print(f"\nTry nuber {repeat_n_times} \n")
+
+        else:
+            print("\n\nrepeat_n_times is not lower than 3 or changer isn't NOT ")
+
+
+Runner()
