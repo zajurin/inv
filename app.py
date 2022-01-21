e257fa8825ebb306f82712340e39cd5bbf974775
Alan Rose <alan_rose@hotmail.es>
2022-01-20 20:47:24 -0600

    adding app.py before creating branch automatically

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from datetime import MAXYEAR
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
-# ------ DOCUMENTATION CODE ------- 
+# ------ DOCUMENTATION CODE -------
 ENVIROMENT = "dev"
+
+db_user = os.environ.get('DB_USER_PSQL')
+db_password = os.environ.get('DB_PASSWORD_PSQL')
+fullCredentials = f"postgresql://{db_user}:{db_password}@localhost/inventory2"
 
 if ENVIROMENT == "dev":
     app.debug = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin1@localhost/inventory2'
+    app.config['SQLALCHEMY_DATABASE_URI'] = fullCredentials
 
 else:
     app.debug = False
@@ -19,13 +24,14 @@ else:
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
+
 class MyModel(db.Model):
     __tablename__ = 'modelInventory'
     id = db.Column(db.Integer, primary_key=True)
-    user = db.Column(db.String(200), unique = True)
+    user = db.Column(db.String(200), unique=True)
     brand = db.Column(db.String(200))
     model = db.Column(db.String(200))
-    serialNumber = db.Column(db.String(200), unique = True)
+    serialNumber = db.Column(db.String(200), unique=True)
     ram = db.Column(db.Float)
     operatingSystem = db.Column(db.String(200))
     licence = db.Column(db.String(200))
@@ -33,17 +39,17 @@ class MyModel(db.Model):
     azureAdPrt = db.Column(db.String(200))
     isDeviceJoined = db.Column(db.String(200))
     isUserAzureAD = db.Column(db.String(200))
-    macAddress = db.Column(db.String(200), unique = True)
+    macAddress = db.Column(db.String(200), unique=True)
 
     def __init__(self, user, brand, model, serialNumber, ram, operatingSystem, licence, azureAdJoined, azureAdPrt, isDeviceJoined, isUserAzureAD, macAddress):
-        self.user= user
+        self.user = user
         self.brand = brand
         self.model = model
         self.serialNumber = serialNumber
         self.ram = ram
         self.operatingSystem = operatingSystem
         self.licence = licence
-        self.azureAdJoined = azureAdJoined         
+        self.azureAdJoined = azureAdJoined
         self.azureAdPrt = azureAdPrt
         self.isDeviceJoined = isDeviceJoined
         self.isUserAzureAD = isUserAzureAD
@@ -69,19 +75,23 @@ def submit():
         azureAdJoined = request.form['azureAdJoined']
         azureAdPrt = request.form['azureAdPrt']
         isDeviceJoined = request.form['isDeviceJoined']
-        isUserAzureAD = request.form['isUserAzureAD']        
+        isUserAzureAD = request.form['isUserAzureAD']
         macAddress = request.form['macAddress']
-        print(user, brand, model, serialNumber, ram, operatingSystem, licence, azureAdJoined, azureAdPrt, isDeviceJoined, isUserAzureAD, macAddress) 
-        inputtList = [user, brand, model, serialNumber, ram, operatingSystem, licence, azureAdJoined, azureAdPrt, isDeviceJoined, isUserAzureAD, macAddress]
+        print(user, brand, model, serialNumber, ram, operatingSystem, licence,
+              azureAdJoined, azureAdPrt, isDeviceJoined, isUserAzureAD, macAddress)
+        inputtList = [user, brand, model, serialNumber, ram, operatingSystem, licence,
+                      azureAdJoined, azureAdPrt, isDeviceJoined, isUserAzureAD, macAddress]
         for oneInput in inputtList:
             if oneInput == '':
-                return render_template('index.html', message = 'Please enter requiered info')
+                return render_template('index.html', message='Please enter requiered info')
         if db.session.query(MyModel).filter(MyModel.user == user).count() == 0:
-            data = MyModel(user, brand, model, serialNumber, ram, operatingSystem, licence, azureAdJoined, azureAdPrt, isDeviceJoined, isUserAzureAD, macAddress)
+            data = MyModel(user, brand, model, serialNumber, ram, operatingSystem, licence,
+                           azureAdJoined, azureAdPrt, isDeviceJoined, isUserAzureAD, macAddress)
             db.session.add(data)
             db.session.commit()
             return render_template('success.html')
         return render_template('success.html', message='You have already Submitted')
+
 
 if __name__ == "__main__":
     app.run()
